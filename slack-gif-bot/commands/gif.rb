5b29f68369df50c9586e215fb46cdceed59d63a2
Chris Kelly <ckelly@newsinc.com>
2016-01-26 13:59:50 -0500

    try giphy

--- slack-gif-bot/commands/gif.rb
+++ slack-gif-bot/commands/gif.rb
@@ -1,11 +1,16 @@
+require 'httparty'
+
 module SlackGifBot
   module Commands
     class Gif < SlackRubyBot::Commands::Base
       def self.call(client, data, _match)
         query = data.text.split('>: ')[1]
-        images = GoogleCustomSearchApi.search(query, {:searchType => 'image', :fileType => 'gifv', :fileType => 'gif'})
-        if images['items'].count > 0
-          image = images['items'].sample.link
+        #Public API Key for now since giphy is in beta
+        response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=#{URI.encode(query)}&api_key=dc6zaTOxFJmzC")
+        images = response['data']
+
+        if images.count > 0
+          image = images.sample['url']
         else
           image = "Unable to find a matching Gif. Try a different query"
         end  
